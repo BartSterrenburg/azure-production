9256b0844be8bc59a544b4ebbaf732a204f5f359
LiamVrs <lvrins@icloud.com>
2024-06-04 21:34:12 +0200

    liam version

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const userRoutes = require('./src/Routes/user.routes');
-const formRoutes = require('./src/Routes/form.routes');
-
+const userRoutes = require("./src/Routes/user.routes");
+const formRoutes = require("./src/Routes/form.routes");
+ 
 // Gebruik CORS middleware
-app.use(cors());
-
+const corsOptions = {
+  origin: "http://127.0.0.1:3000",
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+ 
+app.use(cors(corsOptions));
 app.use(express.json());
-
+ 
 app.get("/", (req, res, next) => {
   res.json({
     status: 200,
@@ -16,11 +21,10 @@ app.get("/", (req, res, next) => {
     data: {},
   });
 });
-
-app.use(userRoutes)
-app.use(formRoutes)
-
-
+ 
+app.use(userRoutes);
+app.use(formRoutes);
+ 
 // Route error handler
 app.use((req, res, next) => {
   next({
@@ -29,7 +33,7 @@ app.use((req, res, next) => {
     data: {},
   });
 });
-
+ 
 // Express error handler
 app.use((error, req, res, next) => {
   res.status(error.status || 500).json({
@@ -38,7 +42,7 @@ app.use((error, req, res, next) => {
     data: {},
   });
 });
-
+ 
 // Onderstaande code zorgt ervoor dat de server alleen luistert als het bestand niet tijdens testen wordt uitgevoerd.
 if (process.env.NODE_ENV !== "test") {
   const port = process.env.PORT || 3000;
@@ -46,5 +50,5 @@ if (process.env.NODE_ENV !== "test") {
     console.log(`Server is running on port ${port}`);
   });
 }
-
+ 
 module.exports = app;
