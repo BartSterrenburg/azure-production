883d6a50c0892c337a2917f233e01353d287d629
LiamVrs <lvrins@icloud.com>
2024-06-04 14:41:14 +0200

    fix

--- dbConnection.js
+++ dbConnection.js
@@ -1,21 +1,20 @@
-const mysql = require('mysql2')
-require('dotenv').config()
-
+const mysql = require("mysql2");
+require("dotenv").config();
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  port: 3306,
-  database: "project",
-  password: "mypass"
-})
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+});
 
 connection.connect((err) => {
   if (err) {
-    console.error('Error connecting to database: ', err)
-    return
+    console.error("Error connecting to database: ", err);
+    return;
   }
-  console.log(`Connected to database.`)
-})
+  console.log(`Connected to database.`);
+});
 
-module.exports = connection
+module.exports = connection;
