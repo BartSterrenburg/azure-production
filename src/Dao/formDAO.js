c92da3be42aa72bb3bb5c05452a0b3d368cfbab1
LiamVrs <lvrins@icloud.com>
2024-06-05 10:42:08 +0200

    add to database wpi method

--- src/Dao/formDAO.js
+++ src/Dao/formDAO.js
@@ -1,16 +1,56 @@
-const database = require('../../dbConnection');
-const queryLibrary = require('./queryCollection');
+const database = require("../../dbConnection");
+const queryLibrary = require("./queryCollection");
 
 const formDAO = {
-    saveWPI: (form, callback) => {
-        database.query(queryLibrary.postWPI, [form.nummer], (err, rows) => {
-            if (err) {
-                console.error("Error executing query", err);
-                return callback(err, null);
-            }
-            callback(null, rows);
-        });
-    }
+  saveWPI: (form, callback) => {
+    database.query(
+      queryLibrary.postWPI,
+      [
+        form.nummer,
+        form.datum,
+        form.project,
+        form.locatie,
+        form.naamEigenaar,
+        form.functieEigenaar,
+        form.inspectie.regelsVoorschriften
+          .gehandeldVolgensRegelsEnVoorschriften,
+        form.inspectie.regelsVoorschriften
+          .gehandeldVolgensRegelsEnVoorschriftenAantekeningen,
+        form.inspectie.omstandigheden.omstandighedenVeiligWerken,
+        form.inspectie.omstandigheden.omstandighedenVeiligWerkenAantekeningen,
+        form.inspectie.uitvoerenden.voldoenUitvoerendeAanEisen,
+        form.inspectie.uitvoerenden.voldoenUitvoerendeAanEisenAantekeningen,
+        form.inspectie.pbmGebruikt.vereisteBeschermingsmiddelen,
+        form.inspectie.pbmGebruikt.vereisteBeschermingsmiddelenAantekeningen,
+        form.inspectie.gevaarlijkeSituaties.gevaarlijkeSituatiesVoorkomen,
+        form.inspectie.gevaarlijkeSituaties
+          .gevaarlijkeSituatiesVoorkomenAantekeningen,
+        form.inspectie.milieubelastendeStoffen.gevaarlijkeStoffenVerwerking,
+        form.inspectie.milieubelastendeStoffen
+          .gevaarlijkeStoffenVerwerkingAantekeningen,
+        form.inspectie.voorzieningenAanwezig.benodigdeVoorzieningenCalimiteiten,
+        form.inspectie.voorzieningenAanwezig
+          .benodigdeVoorzieningenCalimiteitenAantekeningen,
+        form.inspectie.gebruikteGereedschappen.staatGebruiktGereedschappen,
+        form.inspectie.gebruikteGereedschappen
+          .staatGebruiktGereedschappenAantekeningen,
+          form.omschrijvingVerbetering,
+          form.actieTeNemenDoor,
+          form.actieTeNemenVoorDatum,
+          form.evaluatieTerVerbetering,
+          form.datumAfgehandeld,
+          form.paraaf
+
+      ],
+      (err, rows) => {
+        if (err) {
+          console.error("Error executing query", err);
+          return callback(err, null);
+        }
+        callback(null, rows);
+      }
+    );
+  },
 };
 
 module.exports = formDAO;
--- src/Dao/queryCollection.js
+++ src/Dao/queryCollection.js
@@ -1,10 +1,11 @@
-
 const queries = {
-    getAllUsers: "SELECT * FROM gebruiker",
-    getUserById: "SELECT * FROM gebruiker WHERE personeelsnummer = ?",
-    getUserRolByID: "SELECT rol FROM gebruiker WHERE personeelsnummer = ?",
-    getPasswordByID: "SELECT wachtwoord FROM gebruiker WHERE personeelsnummer = ?",
-    postWPI: "INSERT INTO formulier_wpi (formNummer) VALUES (?)",
-  };
-  
-  module.exports = queries;
+  getAllUsers: "SELECT * FROM gebruiker",
+  getUserById: "SELECT * FROM gebruiker WHERE personeelsnummer = ?",
+  getUserRolByID: "SELECT rol FROM gebruiker WHERE personeelsnummer = ?",
+  getPasswordByID:
+    "SELECT wachtwoord FROM gebruiker WHERE personeelsnummer = ?",
+  postWPI:
+    "INSERT INTO formulier_wpi (formNummer, datum, project, locatie, naamEigenaar, functieEigenaar, gehandeldVolgensRegelsEnVoorschriften, gehandeldVolgensRegelsEnVoorschriftenAantekeningen, omstandighedenVeiligWerken, omstandighedenVeiligWerkenAantekeningen, voldoenUitvoerendeAanEisen, voldoenUitvoerendeAanEisenAantekeningen, vereisteBeschermingsmiddelen, vereisteBeschermingsmiddelenAantekeningen, gevaarlijkeSituatiesVoorkomen, gevaarlijkeSituatiesVoorkomenAantekeningen, gevaarlijkeStoffenVerwerking, gevaarlijkeStoffenVerwerkingAantekeningen, benodigdeVoorzieningenCalimiteiten, benodigdeVoorzieningenCalimiteitenAantekeningen, staatGebruiktGereedschappen, staatGebruiktGereedschappenAantekeningen, omschrijvingVerbetering, actieTeNemenDoor, actieTeNemenVoorDatum, evaluatieTerVerbetering, datumAfgehandeld, paraaf) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+};
+
+module.exports = queries;
