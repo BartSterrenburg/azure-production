c05804ad7dd8860b056d893ce347cbea1a1864e0
LiamVrs <lvrins@icloud.com>
2024-06-14 10:33:18 +0200

    made 168h

--- src/Dao/token.js
+++ src/Dao/token.js
@@ -23,7 +23,7 @@ const tokenFunctions = {
 
   createToken: (userId) => {
     const token = jwt.sign(
-      { id: userId }, process.env.JWT_KEY, { expiresIn: "5s" })
+      { id: userId }, process.env.JWT_KEY, { expiresIn: "168h" })
     return token
   },
 }
