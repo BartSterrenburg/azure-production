50a274d3fd37881b8538c41464a51d0fb6fcfd71
LiamVrs <lvrins@icloud.com>
2024-06-14 10:32:40 +0200

    fix bug

--- src/Dao/token.js
+++ src/Dao/token.js
@@ -23,7 +23,7 @@ const tokenFunctions = {
 
   createToken: (userId) => {
     const token = jwt.sign(
-      { id: userId }, process.env.JWT_KEY, { expiresIn: "30s" })
+      { id: userId }, process.env.JWT_KEY, { expiresIn: "5s" })
     return token
   },
 }
